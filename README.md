d8fad9dff18de71496b132baba33cb021e98a926
Steven Snyder <snyder13@bronson.hubzero.org>
2017-12-22 18:43:38 -0500

    tests, README

---
+++ README.md
@@ -0,0 +1,80 @@
+# hex-object
+
+This is a small collection of utilities for dealing with object manipulation, aimed primarily at the use case of configuration objects.
+
+## "Dotted-path" shortcut keys
+This module implements utilities to support a short alternate syntax for deep structures:
+```javascript
+{
+    'login.strategies.twilio.api': {
+        'id': 'foo',
+        'key': 'bar'
+    },
+    'login.strategies.local.store': 'redis'
+}
+// - or -
+{
+    'login.strategies': {
+        'twilio.api': {
+            'id': 'foo',
+            'key': 'bar'
+        },
+        'redis.store': 'redis'
+    }
+}
+```
+can be used in place of the "normalized" JSON equivalent:
+```json
+{
+    "login": { "strategies": {
+         "twilio": { "api": {
+            "id": "foo",
+            "key": "bar"
+         } } 
+     } }
+}
+```
+
+This module's `normalize` converts either of the former into the latter. After an object is normalized, you can use `get`, `set`, `push`, and `concat` to operate it on it using shortcuts:
+
+```javascript
+const obj = require('hex-object'), conf = require('./login-conf');
+obj.normalize(conf);
+obj.get(conf, 'login.strategies.local');
+// { 'store': 'redis' }
+```
+
+### Wrapping
+If you want to do more than one operation on an object, it can be helpful to wrap it:
+```javascript
+const obj = require('hex-object'), conf = obj.wrap(require('./login-conf'));
+conf
+    .normalize()
+    .set('login.strategies.local.store', 'postgres');
+    
+conf.get('login.strategies.local');
+// { 'store': 'postgres' });
+```
+
+*Refer to the tests for more usage examples.*
+
+## Overriding & Augmenting
+
+Sometimes it is necessary to combine configuration from various sources in the same key space.
+
+This is how it works:
+```javascript
+const obj = require('hex-object'),
+     conf = require('./conf'),
+// { 'port': 8000, 'data-source': 'fs', 'plugins': [ 'a', 'b' ] }
+  modConf = require('./mod/conf');
+// { 'plugins': [ 'c' ], 'data-source': 'redis' }
+
+obj.augment(conf, modConf);
+// {
+//     'port': 8000,
+//     'data-source': 'redis', // later-declared value "wins"
+//     'plugins': [ 'a', 'b', 'c' ] // except in cases of arrays, which get merged
+// }
+
+```
---
+++ test/usage.test.js
@@ -0,0 +1,109 @@
+'use strict';
+const lib = require('../');
+
+describe('`get`', () => {
+	const obj = { 'a': { 'b': 42 }, 'c': true };
+	it('returns full object when path is not provided', () => {
+		expect(lib.get(obj)).toBe(obj);
+	});
+	it('returns first-level keys', () => {
+		expect(lib.get(obj, 'a')).toBe(obj.a);
+		expect(lib.get(obj, 'c')).toBe(obj.c);
+	});
+	it('returns deep keys', () => {
+		expect(lib.get(obj, 'a.b')).toBe(obj.a.b);
+	});
+	it('returns a default when the path is not set', () => {
+		expect(lib.get(obj, 'does not exist', -1)).toBe(-1);
+	});
+	it('throws when the path is not set and there is no default', () => {
+		expect(() => {
+			lib.get(obj, 'does not exist');
+		}).toThrow();
+	})
+});
+
+describe('`set`', () => {
+	const obj = {};
+	it('sets first-level keys', () => {
+		lib.set(obj, 'a', 1);
+		expect(obj).toEqual({ 'a': 1 });
+	});
+	it('sets deep keys', () => {
+		lib.set(obj, 'b.c', 42);
+		expect(obj).toEqual({ 'a': 1, 'b': { 'c': 42 } });
+	});
+	it('uses a custom setter when provided', () => {
+		lib.set(obj, 'b.c', null, (targ, key) => { targ[key] = 40; });
+		expect(obj).toEqual({ 'a': 1, 'b': { 'c': 40 } });
+	});
+});
+
+describe('`push`', () => {
+	const obj = {};
+	it('can instantiate first-level keys', () => {
+		lib.push(obj, 'a', 1);
+		expect(obj).toEqual({ 'a': [1] });
+	});
+	it('can instantiate deep keys', () => {
+		lib.push(obj, 'b.c', 2);
+		expect(obj).toEqual({ 'a': [1], 'b': { 'c': [2] } });
+	});
+	it('appends array values', () => {
+		lib.push(obj, 'b.c', [3]);
+		expect(obj).toEqual({ 'a': [1], 'b': { 'c': [2, [3]] } });
+	});
+});
+
+describe('`concat`', () => {
+	const obj = {};
+	it('can instantiate first-level keys', () => {
+		lib.concat(obj, 'a', 1);
+		expect(obj).toEqual({ 'a': [1] });
+	});
+	it('can instantiate deep keys', () => {
+		lib.concat(obj, 'b.c', 2);
+		expect(obj).toEqual({ 'a': [1], 'b': { 'c': [2] } });
+	});
+	it('merges array values', () => {
+		lib.concat(obj, 'b.c', [3]);
+		expect(obj).toEqual({ 'a': [1], 'b': { 'c': [2, 3] } });
+	});
+});
+
+describe('`normalize`', () => {
+	it('converts documents with path-style keys into the deep structures they represent', () => {
+		const obj = {
+			'a': 1,
+			'b.c.d': 2,
+			'e': { 'f.g': 3 },
+			'b.h': 2.5
+		};
+		lib.normalize(obj);
+		expect(obj).toEqual({
+			'a': 1,
+			'b': {
+				'c': { 'd': 2 },
+				'h': 2.5
+			},
+			'e': { 'f': { 'g': 3 } }
+		});
+	});
+});
+
+describe('`wrap`', () => {
+	it('allows chaining the other library methods on an object', () => {
+		expect(lib.wrap({})
+			.set('a', 1)
+			.set('b.c', 2)
+			.push('d', 3)
+			.push('d', 4)
+			.concat('d', [5, 6])
+			.get())
+			.toEqual({
+				'a': 1,
+				'b': { 'c': 2 },
+				'd': [ 3, 4, 5, 6 ]
+			});
+	});
+});
